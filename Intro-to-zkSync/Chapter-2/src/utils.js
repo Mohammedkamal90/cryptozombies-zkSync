a5bb5e276271c05261dfa2f3240035ed3de83456
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2025-01-18 00:29:44 +0200

    Chapter 3

---
+++ Intro-to-zkSync/Chapter-2/src/utils.js
@@ -0,0 +1,11 @@
+// Start here
+async function getZkSyncProvider (zksync, networkName) {
+    let zkSyncProvider
+    try {
+    zkSyncProvider = await zksync.getDefaultProvider(networkName)
+    } catch (error) {
+        console.log('Unable to connect to zkSync.')
+        console.log(error)
+    }
+    return zkSyncProvider
+}
